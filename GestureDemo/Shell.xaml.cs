dccc9c6bb7c9beb4c676c58a999921676190f6b6
Justin Liu <justin.xin.liu@gmail.com>
2015-08-11 00:29:06 +1000

    Extend the SplitView control to allow Swipe from Edge gestures.

---
+++ GestureDemo/Shell.xaml.cs
@@ -0,0 +1,56 @@
+﻿using GestureDemo.Pages;
+using GestureDemo.Presentation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+namespace GestureDemo
+{
+    public sealed partial class Shell : UserControl
+    {
+        public Shell()
+        {
+            this.InitializeComponent();
+
+            var vm = new ShellViewModel();
+            vm.MenuItems.Add(new MenuItem { Icon = "", Title = "Welcome", PageType = typeof(WelcomePage) });
+            vm.MenuItems.Add(new MenuItem { Icon = "", Title = "Page 1", PageType = typeof(Page1) });
+            vm.MenuItems.Add(new MenuItem { Icon = "", Title = "Page 2", PageType = typeof(Page2) });
+            vm.MenuItems.Add(new MenuItem { Icon = "", Title = "Page 3", PageType = typeof(Page3) });
+
+            // select the first menu item
+            vm.SelectedMenuItem = vm.MenuItems.First();
+
+            this.ViewModel = vm;
+
+            // add entry animations
+            var transitions = new TransitionCollection { };
+            var transition = new NavigationThemeTransition { };
+            transitions.Add(transition);
+            this.Frame.ContentTransitions = transitions;
+        }
+
+        public ShellViewModel ViewModel { get; private set; }
+
+        public Frame RootFrame
+        {
+            get
+            {
+                return this.Frame;
+            }
+        }
+    }
+}
---
+++ GestureDemo/SwipeableSplitView.cs
@@ -0,0 +1,367 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Shapes;
+
+namespace GestureDemo
+{
+    public sealed class SwipeableSplitView : SplitView
+    {
+        #region private variables
+
+        Grid _paneRoot;
+        Grid _overlayRoot;
+        Rectangle _panArea;
+        Rectangle _dismissLayer;
+        CompositeTransform _paneRootTransform;
+        CompositeTransform _panAreaTransform;
+        Storyboard _openSwipeablePane;
+        Storyboard _closeSwipeablePane;
+
+        #endregion
+
+        public SwipeableSplitView()
+        {
+            this.DefaultStyleKey = typeof(SwipeableSplitView);
+        }
+
+        #region properties
+
+        // safely subscribe/unsubscribe manipulation events here
+        internal Grid PaneRoot
+        {
+            get { return _paneRoot; }
+            set
+            {
+                if (_paneRoot != null)
+                {
+                    _paneRoot.ManipulationStarted -= OnManipulationStarted;
+                    _paneRoot.ManipulationDelta -= OnManipulationDelta;
+                    _paneRoot.ManipulationCompleted -= OnManipulationCompleted;
+                }
+
+                _paneRoot = value;
+
+                if (_paneRoot != null)
+                {
+                    _paneRoot.ManipulationStarted += OnManipulationStarted;
+                    _paneRoot.ManipulationDelta += OnManipulationDelta;
+                    _paneRoot.ManipulationCompleted += OnManipulationCompleted;
+                }
+            }
+        }
+
+        // safely subscribe/unsubscribe manipulation events here
+        internal Rectangle PanArea
+        {
+            get { return _panArea; }
+            set
+            {
+                if (_panArea != null)
+                {
+                    _panArea.ManipulationStarted -= OnManipulationStarted;
+                    _panArea.ManipulationDelta -= OnManipulationDelta;
+                    _panArea.ManipulationCompleted -= OnManipulationCompleted;
+                }
+
+                _panArea = value;
+
+                if (_panArea != null)
+                {
+                    _panArea.ManipulationStarted += OnManipulationStarted;
+                    _panArea.ManipulationDelta += OnManipulationDelta;
+                    _panArea.ManipulationCompleted += OnManipulationCompleted;
+                }
+            }
+        }
+
+        // safely subscribe/unsubscribe manipulation events here
+        internal Rectangle DismissLayer
+        {
+            get { return _dismissLayer; }
+            set
+            {
+                if (_dismissLayer != null)
+                {
+                    _dismissLayer.Tapped -= OnDismissLayerTapped;
+                }
+
+                _dismissLayer = value;
+
+                if (_dismissLayer != null)
+                {
+                    _dismissLayer.Tapped += OnDismissLayerTapped; ;
+                }
+            }
+        }
+
+        // safely subscribe/unsubscribe animation completed events here
+        internal Storyboard OpenSwipeablePaneAnimation
+        {
+            get { return _openSwipeablePane; }
+            set
+            {
+                if (_openSwipeablePane != null)
+                {
+                    _openSwipeablePane.Completed -= OnOpenSwipeablePaneCompleted;
+                }
+
+                _openSwipeablePane = value;
+
+                if (_openSwipeablePane != null)
+                {
+                    _openSwipeablePane.Completed += OnOpenSwipeablePaneCompleted;
+                }
+            }
+        }
+
+        // safely subscribe/unsubscribe animation completed events here
+        internal Storyboard CloseSwipeablePaneAnimation
+        {
+            get { return _closeSwipeablePane; }
+            set
+            {
+                if (_closeSwipeablePane != null)
+                {
+                    _closeSwipeablePane.Completed -= OnCloseSwipeablePaneCompleted;
+                }
+
+                _closeSwipeablePane = value;
+
+                if (_closeSwipeablePane != null)
+                {
+                    _closeSwipeablePane.Completed += OnCloseSwipeablePaneCompleted;
+                }
+            }
+        }
+
+        public bool IsSwipeablePaneOpen
+        {
+            get { return (bool)GetValue(IsSwipeablePaneOpenProperty); }
+            set { SetValue(IsSwipeablePaneOpenProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsSwipeablePaneOpenProperty =
+            DependencyProperty.Register("IsSwipeablePaneOpen", typeof(bool), typeof(SwipeableSplitView), new PropertyMetadata(false, OnIsSwipeablePaneOpenChanged));
+
+        static void OnIsSwipeablePaneOpenChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var splitView = (SwipeableSplitView)d;
+
+            switch (splitView.DisplayMode)
+            {
+                case SplitViewDisplayMode.Inline:
+                case SplitViewDisplayMode.CompactOverlay:
+                case SplitViewDisplayMode.CompactInline:
+                    splitView.IsPaneOpen = (bool)e.NewValue;
+                    break;
+
+                case SplitViewDisplayMode.Overlay:
+                    if (splitView.OpenSwipeablePaneAnimation == null || splitView.CloseSwipeablePaneAnimation == null) return;
+                    if ((bool)e.NewValue)
+                    {
+                        splitView.OpenSwipeablePane();
+                    }
+                    else
+                    {
+                        splitView.CloseSwipeablePane();
+                    }
+                    break;
+            }
+        }
+
+        public double PanAreaInitialTranslateX
+        {
+            get { return (double)GetValue(PanAreaInitialTranslateXProperty); }
+            set { SetValue(PanAreaInitialTranslateXProperty, value); }
+        }
+
+        public static readonly DependencyProperty PanAreaInitialTranslateXProperty =
+            DependencyProperty.Register("PanAreaInitialTranslateX", typeof(double), typeof(SwipeableSplitView), new PropertyMetadata(0d));
+
+        public double PanAreaThreshold
+        {
+            get { return (double)GetValue(PanAreaThresholdProperty); }
+            set { SetValue(PanAreaThresholdProperty, value); }
+        }
+
+        public static readonly DependencyProperty PanAreaThresholdProperty =
+            DependencyProperty.Register("PanAreaThreshold", typeof(double), typeof(SwipeableSplitView), new PropertyMetadata(36d));
+
+        #endregion
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            PaneRoot = GetTemplateChild("PaneRoot") as Grid;
+            _overlayRoot = GetTemplateChild("OverlayRoot") as Grid;
+            PanArea = GetTemplateChild("PanArea") as Rectangle;
+            DismissLayer = GetTemplateChild("DismissLayer") as Rectangle;
+
+            if (PaneRoot == null || _overlayRoot == null || PanArea == null || DismissLayer == null)
+            {
+                throw new ArgumentException("Make sure you have copied the default style to Generic.xaml!!");
+            }
+
+            var rootGrid = _paneRoot.Parent as Grid;
+
+            if (rootGrid == null)
+            {
+                throw new ArgumentException("Make sure you have copied the default style to Generic.xaml!!");
+            }
+
+            OpenSwipeablePaneAnimation = rootGrid.Resources["OpenSwipeablePane"] as Storyboard;
+            CloseSwipeablePaneAnimation = rootGrid.Resources["CloseSwipeablePane"] as Storyboard;
+
+            if (OpenSwipeablePaneAnimation == null || CloseSwipeablePaneAnimation == null)
+            {
+                throw new ArgumentException("Make sure you have copied the default style to Generic.xaml!!");
+            }
+
+            // initialization
+            OnDisplayModeChanged(null, null);
+
+            RegisterPropertyChangedCallback(DisplayModeProperty, OnDisplayModeChanged);
+
+            // disable ScrollViewer as it will prevent finger from panning
+            if (Pane is ListView || Pane is ListBox)
+            {
+                ScrollViewer.SetVerticalScrollMode(Pane, ScrollMode.Disabled);
+            }
+        }
+
+        #region native property change handlers
+
+        void OnDisplayModeChanged(DependencyObject sender, DependencyProperty dp)
+        {
+            switch (DisplayMode)
+            {
+                case SplitViewDisplayMode.Inline:
+                case SplitViewDisplayMode.CompactOverlay:
+                case SplitViewDisplayMode.CompactInline:
+                    PanAreaInitialTranslateX = 0d;
+                    _overlayRoot.Visibility = Visibility.Collapsed;
+                    break;
+
+                case SplitViewDisplayMode.Overlay:
+                    PanAreaInitialTranslateX = OpenPaneLength * -1;
+                    _overlayRoot.Visibility = Visibility.Visible;
+                    break;
+            }
+
+            if (sender != null)
+            {
+                CloseSwipeablePane();
+            }
+        }
+
+        #endregion
+
+        #region manipulation event handlers
+
+        void OnManipulationStarted(object sender, ManipulationStartedRoutedEventArgs e)
+        {
+            _panAreaTransform = PanArea.RenderTransform as CompositeTransform;
+            _paneRootTransform = PaneRoot.RenderTransform as CompositeTransform;
+
+            if (_panAreaTransform == null || _paneRootTransform == null)
+            {
+                throw new ArgumentException("Make sure you have copied the default style to Generic.xaml!!");
+            }
+        }
+
+        void OnManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+            var x = _panAreaTransform.TranslateX + e.Delta.Translation.X;
+
+            // keep the pan within the bountry
+            if (x < PanAreaInitialTranslateX || x > 0) return;
+
+            // while we are panning the PanArea on X axis, let's sync the PaneRoot's position X too
+            _paneRootTransform.TranslateX = _panAreaTransform.TranslateX = x;
+        }
+
+        void OnManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
+        {
+            var x = e.Velocities.Linear.X;
+
+            // ignore a little bit velocity (+/-0.1)
+            if (x <= -0.1)
+            {
+                CloseSwipeablePane();
+            }
+            else if (x > -0.1 && x < 0.1)
+            {
+                if (Math.Abs(_panAreaTransform.TranslateX) > Math.Abs(PanAreaInitialTranslateX) / 2)
+                {
+                    CloseSwipeablePane();
+                }
+                else
+                {
+                    OpenSwipeablePane();
+                }
+            }
+            else
+            {
+                OpenSwipeablePane();
+            }
+        }
+
+        #endregion
+
+        #region DismissLayer tap event handlers
+
+        void OnDismissLayerTapped(object sender, TappedRoutedEventArgs e)
+        {
+            CloseSwipeablePane();
+        }
+
+        #endregion
+
+        #region animation completed event handlers
+
+        void OnOpenSwipeablePaneCompleted(object sender, object e)
+        {
+            this.DismissLayer.IsHitTestVisible = true;
+        }
+
+        void OnCloseSwipeablePaneCompleted(object sender, object e)
+        {
+            this.DismissLayer.IsHitTestVisible = false;
+        }
+
+        #endregion
+
+        #region private methods
+
+        void OpenSwipeablePane()
+        {
+            if (IsSwipeablePaneOpen)
+            {
+                OpenSwipeablePaneAnimation.Begin();
+            }
+            else
+            {
+                IsSwipeablePaneOpen = true;
+            }
+        }
+
+        void CloseSwipeablePane()
+        {
+            if (!IsSwipeablePaneOpen)
+            {
+                CloseSwipeablePaneAnimation.Begin();
+            }
+            else
+            {
+                IsSwipeablePaneOpen = false;
+            }
+        }
+
+        #endregion
+    }
+}
