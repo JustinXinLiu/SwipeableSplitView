b5e1cdab09d64ba745a82a30acbca539626f59eb
Marco Loiodice <mloiodice13@gmail.com>
2016-04-10 11:47:42 +0200

    Changes for use Symbol enum
    replace Segoe MDL2 Assets string by Symbol enum for easy choice icon.

--- GestureDemo/Shell.xaml.cs
+++ GestureDemo/Shell.xaml.cs
@@ -1,21 +1,8 @@
 ﻿using GestureDemo.Pages;
 using GestureDemo.Presentation;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Animation;
-using Windows.UI.Xaml.Navigation;
 
 namespace GestureDemo
 {
@@ -26,10 +13,10 @@ namespace GestureDemo
             this.InitializeComponent();
 
             var vm = new ShellViewModel();
-            vm.MenuItems.Add(new MenuItem { Icon = "", Title = "Welcome", PageType = typeof(WelcomePage) });
-            vm.MenuItems.Add(new MenuItem { Icon = "", Title = "Page 1", PageType = typeof(Page1) });
-            vm.MenuItems.Add(new MenuItem { Icon = "", Title = "Page 2", PageType = typeof(Page2) });
-            vm.MenuItems.Add(new MenuItem { Icon = "", Title = "Page 3", PageType = typeof(Page3) });
+            vm.MenuItems.Add(new MenuItem { IconPage = Symbol.Emoji2, Title = "Welcome", PageType = typeof(WelcomePage) });
+            vm.MenuItems.Add(new MenuItem { IconPage = Symbol.Setting, Title = "Page 1", PageType = typeof(Page1) });
+            vm.MenuItems.Add(new MenuItem { IconPage = Symbol.Street, Title = "Page 2", PageType = typeof(Page2) });
+            vm.MenuItems.Add(new MenuItem { IconPage = Symbol.World, Title = "Page 3", PageType = typeof(Page3) });
 
             // select the first menu item
             vm.SelectedMenuItem = vm.MenuItems.First();
